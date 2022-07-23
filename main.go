9b6c43565111f750af052ce4cdaf30fd7a2d7dfc
Felipe Sales <felipe.ferreira.sales@gmail.com>
2022-07-23 16:45:48 -0300

    complete card repository

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/ffsales/go-trello-poc/config"
 	"github.com/ffsales/go-trello-poc/db"
@@ -18,16 +19,61 @@ func main() {
 	conn := db.GetConnection()
 	defer conn.Close()
 
-	fmt.Println(conn.Ping())
+	// fmt.Println(conn.Ping())
 
-	testInsertCard(conn)
-
+	// testInsertCard(conn)
+	// testGetCard(conn)
+	// testGetAllCards(conn)
+	// testUpdateCard(conn)
+	// testDeleteCard(conn)
 }
 
 func testInsertCard(conn *sql.DB) {
+	var card models.Card = models.Card{
+		Name:     "Teste",
+		Finished: true,
+	}
+
+	newCard, err := repository.Insert(conn, card)
+	fmt.Println(err)
+	fmt.Println(newCard)
+}
+
+func testGetCard(conn *sql.DB) {
+
+	card, err := repository.Get(conn, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(card)
+}
+
+func testGetAllCards(conn *sql.DB) {
+	cards, err := repository.GetAll(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cards)
+}
+
+func testUpdateCard(conn *sql.DB) {
 	card := new(models.Card)
-	card.Name = "Inicio"
+	card.Id = 1
+	card.Name = "Meio"
 	card.Finished = true
 
-	repository.Insert(conn, *card)
+	rows, _ := repository.Update(conn, card)
+	fmt.Println(rows)
+
+	newCard, _ := repository.Get(conn, int(card.Id))
+	fmt.Println(newCard)
+}
+
+func testDeleteCard(conn *sql.DB) {
+
+	fmt.Println(repository.GetAll(conn))
+
+	repository.Delete(conn, 7)
+
+	fmt.Println(repository.GetAll(conn))
 }
--- repository/card_repository.go
+++ repository/card_repository.go
@@ -2,28 +2,71 @@ package repository
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
 
 	"github.com/ffsales/go-trello-poc/models"
 )
 
-func Insert(conn *sql.DB, card models.Card) error {
+func Insert(conn *sql.DB, card models.Card) (models.Card, error) {
 
-	stmt, errPrepare := conn.Prepare("insert into card(name, finished) values (?, ?)")
-	returnError(errPrepare)
+	stmt, err := conn.Prepare("insert into card(name, finished) values (?, ?)")
+	returnError(err)
 	res, err := stmt.Exec(card.Name, card.Finished)
 	returnError(err)
 
 	card.Id, err = res.LastInsertId()
 
-	fmt.Println(card)
-
-	return err
+	return card, err
 }
 
-func returnError(err error) error {
-	if err != nil {
-		return err
+func Get(conn *sql.DB, id int) (models.Card, error) {
+
+	row := conn.QueryRow("select id, name, finished from card where id = ?", id)
+	card := new(models.Card)
+
+	err := row.Scan(&card.Id, &card.Name, &card.Finished)
+	returnError(err)
+
+	return *card, err
+}
+
+func GetAll(conn *sql.DB) ([]models.Card, error) {
+
+	rows, err := conn.Query("select id, name, finished from card")
+	returnError(err)
+	defer rows.Close()
+
+	var cards []models.Card
+
+	for rows.Next() {
+		var card models.Card
+		rows.Scan(&card.Id, &card.Name, &card.Finished)
+		cards = append(cards, card)
 	}
-	return nil
+	return cards, err
+}
+
+func Update(conn *sql.DB, card *models.Card) (int64, error) {
+	res, err := conn.Exec("update card set name = ?, finished = ? where id = ?", card.Name, card.Finished, card.Id)
+	returnError(err)
+
+	rows, err := res.RowsAffected()
+
+	return rows, err
+}
+
+func Delete(conn *sql.DB, id int) (rows int64, err error) {
+	res, err := conn.Exec("delete from card where id = ?", id)
+	returnError(err)
+
+	rows, err = res.RowsAffected()
+
+	return
+}
+
+func returnError(err error) {
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 }
